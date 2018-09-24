40dada5c4198db2f6b6c9be3d4b63a114d638cd2
Robin Tram <robin.tram@gmail.com>
2018-09-24 18:50:37 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -15,7 +15,7 @@ def key_for_min_value(name_hash)
       end
     end 
     min_value = array[0]
-    name_hash.each do |key, value|
+    name_hash.select do |key, value|
       value == min_value
       return key
     end 
