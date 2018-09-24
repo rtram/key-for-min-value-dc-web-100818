a3b19b17f592895a5f16be18886d6c565161919f
Robin Tram <robin.tram@gmail.com>
2018-09-24 18:48:46 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,20 +3,21 @@
 
 def key_for_min_value(name_hash)
   if name_hash != nil
-  array = []
-  name_hash.collect do |key, value|
-    array << value 
-  end 
-  while array.length > 1
-    if array[0] < array[-1]
-      array.pop
-    elsif array[0] > array[-1]
-      array.shift
-    end
-  end 
-  min_value = array[0]
-  name_hash.each do |key, value|
-    value == min_value
-    return key
+    array = []
+    name_hash.collect do |key, value|
+      array << value 
+    end 
+    while array.length > 1
+      if array[0] < array[-1]
+        array.pop
+      elsif array[0] > array[-1]
+        array.shift
+      end
+    end 
+    min_value = array[0]
+    name_hash.each do |key, value|
+      value == min_value
+      return key
+    end 
   end 
 end
